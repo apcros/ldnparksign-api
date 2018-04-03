c21d5a1401666381bc84afbdca6c2f5d781edaa2
Jules Decol <apcros.mail@gmail.com>
2018-04-03 22:13:22 +0100

    Poor first attempt at parsing the text into a dict

--- parking_sign_parser.py
+++ parking_sign_parser.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import pytesseract
 import cv2
+import calendar
 import tempfile
 import os
 
@@ -26,6 +27,7 @@ class ParkingSignParser:
         #temp_file.close()
         #os.remove(temp_file_name)
 
+        self.image_string = text;
         parsed_data = self._parse_string(text)
         print(parsed_data)
         return text
@@ -37,8 +39,60 @@ class ParkingSignParser:
          #TODO : Detect the parking sign area and crop it
          return image[200:450, 325:600]
 
+    # Because OCR parsing is, by definition, really flaky we need
+    # to include a maximum of safe guards in this parsing method
     @staticmethod
     def _parse_string(image_string):
-        parking_infos = {}
-        
+        parking_infos = {
+            'ranges' : [],
+            'single_days': []
+        }
+
+        # London parking signs use alternatively short and long names
+        days = list(calendar.day_abbr) + list(calendar.day_name)
+
+        matched_days = {}
+        for day in days:
+            match = image_string.find(day)
+
+            if match != -1 and match not in matched_days:
+                matched_days[match] = day
+
+        print(matched_days)
+
+        CHAR_COUNT_IN_SEP = 3
+
+        for index, match_index in enumerate(matched_days):
+            print(index)
+
+            next_index = index + 1
+
+            day_name = matched_days[index].items()[0]
+            print(day_name)
+
+            if next_index >= len(matched_days):
+                parking_infos['single_days'].append(({
+                    'day': day_name
+                }))
+                continue
+
+            if match_index + CHAR_COUNT_IN_SEP + len(day_name) == matched_days.get(next_index).items()[0]:
+                parking_infos['ranges'].append({
+                    'from': {
+                        'day': day_name,
+                        'time': ''
+                    },
+                    'to': {
+                        'day': matched_days[index+1],
+                        'time': ''
+                    }
+                })
+                # We just found a day range
+
+
+        print(parking_infos)
+        print(matched_days)
         return parking_infos
+
+
+
--- tests/test_parking_sign_parser.py
+++ tests/test_parking_sign_parser.py
@@ -1,10 +1,14 @@
 import unittest
+import os
+
 from parking_sign_parser import ParkingSignParser
 
 
 class TestParkingSignParser(unittest.TestCase):
 
     def test_image_to_string(self):
-        sign_parser = ParkingSignParser('/home/apcros/PycharmProjects/ldnparksign-api/tests/fixtures/sign-1.png')
+
+        # OpenCV needs the full path
+        sign_parser = ParkingSignParser(os.getcwd()+'/fixtures/sign-1.png')
 
         self.assertEquals(sign_parser.image_to_string(), 'Super test')
